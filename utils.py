cf23ea1218fb40b63617b6bad870b613791125ba
saugat <saugat.ghimire@maitriservices.com>
2025-08-12 06:03:07 +0000

    update KPI metrics and encounter type on encounter group bar click

--- utils.py
+++ utils.py
@@ -6,6 +6,13 @@ from data.db_query import query_sqlite
 def dt_to_yyyymm(dt):
     return dt.year * 100 + dt.month
 
+def fade_color(hex_color, alpha=0.2):
+    """Convert hex color (#RRGGBB) to RGBA string with given alpha."""
+    r = int(hex_color[1:3], 16)
+    g = int(hex_color[3:5], 16)
+    b = int(hex_color[5:7], 16)
+    return f'rgba({r},{g},{b},{alpha})'
+
 @lru_cache(maxsize=2)
 def load_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Load and cache claims and member data from Snowflake."""
